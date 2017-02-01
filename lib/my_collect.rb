390a0f24d19e22e38c9508cfe2b14972103c6f79
Gee Li <geeyung.li@gmail.com>
2017-02-01 14:31:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+    i = 0
+    collect=[]
+    while i < array.length
+      collect << yield(array[i]) #the yield is just creating the "block" of collection
+      i+=1
+    end
+  collect
+end
+  
 
